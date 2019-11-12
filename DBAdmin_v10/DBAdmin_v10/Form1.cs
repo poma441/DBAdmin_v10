a84614c46dec533a82506274498b299b7e4feed9
poma441 <mysaevpoma@gmail.com>
2019-11-12 22:53:00 +0300

    Added model interface

--- DBAdmin_v10/DBAdmin_v10/Form1.cs
+++ DBAdmin_v10/DBAdmin_v10/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace DBAdmin_v10
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form, IAppView
     {
         public Form1()
         {
