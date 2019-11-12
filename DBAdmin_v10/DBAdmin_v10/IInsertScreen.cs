46b4a2f610cc8929e23fd9f813621bed26a2418b
poma441 <mysaevpoma@gmail.com>
2019-11-13 00:08:27 +0300

    Changed Presenter.cs, added some intefaces

---
+++ DBAdmin_v10/DBAdmin_v10/IInsertScreen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBAdmin_v10
+{
+    public interface IInsertScreen
+    {
+        string Login();
+        string Password();
+        string Name();
+        string Surname();
+        string Patronymic();
+        string Position();
+    }
+}
--- DBAdmin_v10/DBAdmin_v10/Presenter.cs
+++ DBAdmin_v10/DBAdmin_v10/Presenter.cs
@@ -10,6 +10,9 @@ namespace DBAdmin_v10
     {
         IAppView _appView;
         IDB_Model _dB_Model;
+        IInsertScreen _insertScreen;
+        IDeleteScreen _deleteScreen;
+        IChangeInfoScreen _changeInfoScreen;
 
         public Presenter(IAppView appView, IDB_Model dB_Model)
         {
@@ -17,19 +20,31 @@ namespace DBAdmin_v10
             _dB_Model = dB_Model;
         }
 
-        public void CreateUserFromInsertView()
+        public void CreateUserFromInsertScreen()
+        {
+            Users user = new Users();
+            user.login = _insertScreen.Login();
+            user.password = _insertScreen.Password();
+            user.surname = _insertScreen.Surname();
+            user.name = _insertScreen.Name();
+            user.patronymic = _insertScreen.Patronymic();
+            user.position = _insertScreen.Position();
+        }
+
+        public void CreateUserFromDeleteScreen()
         {
 
         }
 
-        public void CreateUserFromDeleteView()
+        public void CreateUserFromChangeInfoScreen()
         {
-
-        }
-
-        public void CreateUserFromChangeInfoView()
-        {
-
+            Users user = new Users();
+            user.login = _changeInfoScreen.Login();
+            user.password = _changeInfoScreen.Password();
+            user.surname = _changeInfoScreen.Surname();
+            user.name = _changeInfoScreen.Name();
+            user.patronymic = _changeInfoScreen.Patronymic();
+            user.position = _changeInfoScreen.Position();
         }
     }
 }
